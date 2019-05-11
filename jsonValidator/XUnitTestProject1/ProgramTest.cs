ecc33bbc26d0289e0b15ebb42c4209f1ddea8bd6
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-05-11 18:30:55 +0300

    Finish JSON evaluator for string

--- jsonValidator/XUnitTestProject1/ProgramTest.cs
+++ jsonValidator/XUnitTestProject1/ProgramTest.cs
@@ -7,10 +7,46 @@ namespace jsonValidator
     public class ProgramTest
     {
         [Fact]
-        public void ASimpleJson()
+        public void JSONThatStartsAndEndsWithQuotationmarks()
         {
             var v = "\"This is a Json\"";
             bool expected = true;
+            bool actual = Program.ValidateJson(v);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void JSONThatStartsAndEndsWithQuotationmarksAndContainsOtherUNICODEChars()
+        {
+            var v = "\"Test\\u0097\nAnother line\"";
+            bool expected = true;
+            bool actual = Program.ValidateJson(v);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void JSONThatOnlyEndsWithQuotationmarks()
+        {
+            var v = "Test\"";
+            bool expected = false;
+            bool actual = Program.ValidateJson(v);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void JSONThatOnlyStartsWithQuotationmarks()
+        {
+            var v = "\"Test";
+            bool expected = false;
+            bool actual = Program.ValidateJson(v);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void JSONWithQuotationMarksAtStartMiddleAndEnd()
+        {
+            var v = "\"Te\"st\"";
+            bool expected = false;
             bool actual = Program.ValidateJson(v);
             Assert.Equal(expected, actual);
         }
--- jsonValidator/jsonValidator/Program.cs
+++ jsonValidator/jsonValidator/Program.cs
@@ -7,20 +7,75 @@ namespace jsonValidator
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
-            ValidateJson(input);
+             bool result = ValidateJson(input);
+            if (result == true)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
             Console.Read();
         }
 
         public static bool ValidateJson(string input)
         {
-            foreach (char c in input)
+            bool result = true;
+            char quotationmark = '"';
+            char reverseSolidus = '\\';
+            char backspace = '\b';
+            char formfeed = '\f';
+            char newLine = '\n';
+            char carriageReturn = '\r';
+            char horizontalTab = '\t';
+
+           for(int i = 0; i < input.Length; i++)
             {
-                if (input[0] == '"' && input[input.Length - 1] == '"')
+                switch (input[i])
                 {
-                    return true;
+                    case '"':
+                        if ( i == 0 || input[i] - 1 == reverseSolidus ||  i == input.Length - 1)
+                        {
+                            result = true;
+                            continue;
+                        }
+                        else
+                        {
+                            result = false;
+                            return result;
+                        }
+                    case '\\':
+                        if (input[0] == '\\')
+                        {
+                            result = false;
+                            return result;
+                        }
+                        else
+                        {
+                            if(input[i] - 1 == reverseSolidus || input[i] + 1 == reverseSolidus
+                            || input[i] + 1 == quotationmark || input[i] + 1 == backspace || input[i] + 1 == formfeed
+                            || input[i] + 1 == newLine || input[i] + 1 == carriageReturn || input[i] + 1 == horizontalTab)
+                            {
+                                result = true;
+                                continue;
+                            }
+                        }
+                        return result;
+                    default:
+                        if (input[0] == quotationmark && input[input.Length - 1] == quotationmark)
+                        {
+                            result = true;
+                        }
+                        else
+                        {
+                            result = false;
+                            return result;
+                        }
+                        continue;
                 }
             }
-            return false;
+            return result;
         }
     }
 }
